0e45106a29b31336d9005b5724a71975416595d1
Yu Feng <yuf107@ucsd.edu>
2018-02-11 18:04:59 -0800

    Url pass param

--- routes/addEvent.js
+++ routes/addEvent.js
@@ -1,8 +1,13 @@
 var data = require('../data.json');
 
 exports.addEvent = function(req, res) {
-        var newEvent = {"name": req.query.name};
-        console.log(data);
+        var newEvent = {"name": req.query.name,
+		"comment": req.query.comment,
+		"timeFrom": req.query.timeFrom,
+		"timeTo": req.query.timeTo,
+		"place": req.query.place};
+
         data.events.push(newEvent);
+	console.log(data);
 }
 
