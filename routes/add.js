b800f490c572f42b286b1370074bb3a727f7dbfe
Yu Feng <yuf107@ucsd.edu>
2018-02-12 14:07:26 -0800

    Begin with edit event

--- routes/add.js
+++ routes/add.js
@@ -2,8 +2,23 @@ var data = require('../data.json');
 
 exports.view = function(req, res){
 	url = req.originalUrl;
-	res.render('add', data)
-	console.log(url);
+	var name = url.substring(5,);
+	name = name.replace(/%20/g, " ");
+
+	var array = data.events;
+	var current_event = {};
+
+	for (var i = 0; i < array.length; i++) {
+		console.log(name);
+		console.log(array[i].name);
+		if (name.valueOf() == array[i].name.valueOf()){
+			console.log("1");
+			current_event = array[i];
+		}
+	}
+
+	res.render('add', current_event);
+	console.log(current_event);
 }
 
 
