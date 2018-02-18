cda54a94266c466c15bbb3c3a6e3fe5472bf6824
Youli Pan <yopan@ucsd.edu>
2018-02-18 15:46:21 -0800

    Add time and date selection

--- routes/addEvent.js
+++ routes/addEvent.js
@@ -3,6 +3,7 @@ var data = require('../data.json');
 exports.addEvent = function(req, res) {
         var newEvent = {"name": req.query.name,
 		"comment": req.query.comment,
+		"date": req.query.date,
 		"timeFrom": req.query.timeFrom,
 		"timeTo": req.query.timeTo,
 		"place": req.query.place,
